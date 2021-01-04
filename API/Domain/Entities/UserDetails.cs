8d64138274f47de6f3a6f538918b7fb0e991474b
Ayan Manna <amanna@deloitte.com>
2021-01-04 18:51:49 +0000

    Added Domain project

---
+++ API/Domain/Entities/UserDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public partial class UserDetails : BaseEntity
+    {
+        public UserDetails()
+        {
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string EmailId { get; set; }
+        public string AuthKey { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string FullName { get; set; }
+        public string Organization { get; set; }
+        public string Telephone { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
