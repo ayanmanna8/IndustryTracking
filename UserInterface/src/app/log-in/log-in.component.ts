3b6a7e50ddc9ddd071812d25e82d3e8f09113cac
diptyajitdutta <diptyajit.dutt@gmail.com>
2021-01-09 01:58:20 +0530

    commit

--- UserInterface/src/app/log-in/log-in.component.ts
+++ UserInterface/src/app/log-in/log-in.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.scss']
+  styleUrls: ['./log-in.component.scss'],
+  animations: [
+    
+  ]
 })
 export class LogInComponent implements OnInit {
 
   username = '';
   password = '';
   hide = true;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  logIn(){
   }
 
 }
